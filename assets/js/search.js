d7961e47d3d1aab1700020380d1698e23bff5603
Brian McVeigh <bcmcveigh@gmail.com>
2019-12-26 10:20:43 -0500

    Search: functionality for when user clicks enter key on selected search result.

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,8 +1,67 @@
+/**
+ * Functionality for global search.
+ */
+
 const searchResults = document.querySelector('.search-wrap .search-results ul');
 
 const worker = {
-    provideUpDownArrowFunctionality: function(vueInstance) {
-        // todo
+    provideUpDownArrowFunctionality: function() {
+        // Get the selected search result or the first search result if nothing has been selected.
+        // todo: check validity of the selectors.
+        let selected = document.querySelector('.search-results .search-result a.selected');
+
+        // If nothing has been selected, highlight the first search result.
+        if (!selected) {
+            selected = document.querySelector('.search-results .search-result:first-child a');
+        }
+
+        // If still nothing, don't do anything else because most likely there are no search results.
+        if (!selected) {
+            return;
+        }
+
+        // If there is no selected class, we need to
+        // add it because nothing has been selected.
+        if (!selected.classList.contains('selected')) {
+          selected.classList.add('selected');
+        }
+
+        switch (event.which) {
+          // If the user pressed the down arrow,
+          // select the next search result.
+          case 40:
+            selected.classList.remove('selected');
+
+            if (!selected.parentElement || !selected.parentElement.nextSibling) {
+                return;
+            }
+
+            // Get the next link if it exists so we can highlight it.
+            const nextLink = selected.parentElement.nextSibling.querySelector('a');
+
+            if (nextLink) {
+                nextLink.classList.add('selected');
+            }
+            break;
+
+          // If the user pressed the up arrow,
+          // select the previous search result.
+           case 38:
+            selected.classList.remove('selected');
+
+            if (!selected.parentElement || !selected.parentElement.previousSibling) {
+                return;
+            }
+
+             // Get the previous link if it exists so we can highlight it.
+             const prevLink = selected.parentElement.previousSibling.querySelector('a');
+
+             if (prevLink) {
+                  prevLink.classList.add('selected');
+                  break;
+             }
+            
+        }
     },
 
     handleSearchClick: async function() {
@@ -15,6 +74,26 @@ const worker = {
             .catch(error => {
                 // catch error.
             });
+    },
+
+    handleKeyUp: function(event) {
+        // If the enter key is pressed, go to the url of the
+        // selected search result.
+        if (event.which === 13) {
+            const selected = document
+            .querySelector('.search-results .search-result .selected');
+
+            if (!selected) {
+                return;
+            }
+
+            const destination = selected
+                .attributes
+                .href
+                .value;
+
+            window.location = destination;
+        }
     },
 
     handleKeyDown: function(event) {
